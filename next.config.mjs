18db8a1db32f6a104913b87fd868443cd15bb50a
oumaima ettalbi <talbioumaima853@gmail.com>
2026-04-18 15:22:52 +0200

    Add redirect from root to /home

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,16 @@ const nextConfig = {
     ],
   },
 
+   async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ]
+  },
+
 };
 
 export default nextConfig;
